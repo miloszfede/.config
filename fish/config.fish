3059c0cbb66459f13cd0e59970c2cda50d7e525b
miloszfede <miloszfede@gmail.com>
2025-05-27 22:20:32 +0200

    fzf, yazi

--- fish/config.fish
+++ fish/config.fish
@@ -3,10 +3,15 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Starship prompt
 starship init fish | source
-
+zoxide init fish | source
+fzf --fish | source
 # Bun config
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# Yazi config 
+set -Ux PATH $HOME/.cargo/bin $PATH
+alias y='yazi'
 
 # Vim alias
 function vim
