f232593f16eca3926248f983f700d190a674667d
miloszfede <miloszfede@gmail.com>
2025-05-27 21:41:31 +0200

    Added fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    starship init fish | source
+  #Commands to run in interactive sessions can go here
+  starship init fish | source
 end
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
