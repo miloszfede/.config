1edf6f3a772f7518f6c4da5d22194ce6ff3c43d0
miloszfede <miloszfede@gmail.com>
2025-05-27 21:52:46 +0200

    Added tmux

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,21 @@
-if status is-interactive
-  #Commands to run in interactive sessions can go here
-  starship init fish | source
-end
-
+# Set up environment
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# bun
+# Starship prompt
+starship init fish | source
+
+# Bun config
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+# Vim alias
 function vim
     nvim $argv
 end
+
+# Start tmux automatically if not already in tmux and in interactive session
+if status is-interactive
+    and not set -q TMUX
+    exec tmux
+end
+
